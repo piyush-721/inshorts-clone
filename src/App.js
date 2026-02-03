cf4c9a0c9b951efadcd278e4320664b1433b3d81
Piyush Shinde <workwithpiyush77@gmail.com>
2026-02-03 13:33:09 +0530

    testing top tabs component on mobile

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import TopTabs from './components/home/TopTabs';
+
+function App() {
+  return (
+    <div className="min-vh-100 bg-light">
+      <div className="container-fluid px-0">
+        <TopTabs />
+      </div>
+    </div>
+  );
+}
+
+export default App;
